750facebab70303692c750a890373a1a926a39da
Yannick Yvin <yannickyvin@gmail.com>
2020-03-29 17:25:10 +0200

    correction background

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { dayOfYear } from '../services/dateService'
-
-const background = ((process.env.REACT_APP_PICSUM_BACKGROUND === undefined) || process.env.REACT_APP_PICSUM_BACKGROUND === true ) ? `url('https://picsum.photos/id/${dayOfYear()}/1920/1200')` : `url('fab${dayOfYear() % 52}.jpg')`
-
+const background = ((process.env.REACT_APP_PICSUM_BACKGROUND === undefined) || (process.env.REACT_APP_PICSUM_BACKGROUND)) ? `url('https://picsum.photos/id/${dayOfYear()}/1920/1200')` : `url('fab${dayOfYear() % 52}.jpg')`
 const Layout = ({ children }) => (
   <div className='cont w-100 d-flex justify-content-center' style={{ backgroundImage: background }}>
     {children}
